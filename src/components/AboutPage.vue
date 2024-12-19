31b788933b181703809ce78511422a3a793276c8
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2024-12-19 22:11:48 +0200

    Add "CalloutComponent.vue" and instances in "AboutPage.vue"

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,19 +1,241 @@
-<template>
-  <HeaderComponent />
-  <FooterComponent />
-</template>
-
 <script>
 import HeaderComponent from '../components/HeaderComponent.vue'
-import FooterComponent from './FooterComponent.vue'
+import FooterComponent from '../components/FooterComponent.vue'
+import CalloutComponent from './CalloutComponent.vue'
 
 export default {
   name: 'AboutPage',
   components: {
     HeaderComponent,
     FooterComponent,
+    CalloutComponent,
+  },
+  data() {
+    return {
+      calloutStatusTitle: "I'm Open to New Work Opportunities",
+      calloutStatusMessage:
+        '     I am seeking an internship and a DevOps course, where I can gain hands-on experience with essential tools and practices. I’m eager to apply what I learn in a real-world setting and further develop my skills in the DevOps and Software Engineering field.',
+      calloutConnectTitle: 'How Can I Help to Optimize your Value Stream?',
+      calloutConnectMessage:
+        "Let's eliminate turbulence in your value stream and create a laminar value flow.",
+      // calloutInfoIcon: '../assets/icons/mail.svg#mail',
+    }
   },
 }
 </script>
 
-<style scoped></style>
+<template>
+  <div class="layout">
+    <HeaderComponent />
+    <main class="about">
+      <section>
+        <h1>About Me</h1>
+        <p>
+          I’m Max, a DevOps-aspiring generalist with a design-driven mindset. I consider myself a
+          big-picture thinker who enjoys exploring connections and patterns in a broader context.
+          But I also love diving into specific areas, often getting lost down rabbit holes that
+          satisfy my curiosity and inspire my creativity.
+        </p>
+        <p>
+          <b
+            >My goal is to help create seamless, <em>laminar value streams</em> by reducing process
+            turbulence and allowing ideas to flow effortlessly from concept through continuous
+            delivery to the end user.</b
+          >
+          This enables innovation through optimized developer experiences and delivers meaningful
+          user experiences.
+        </p>
+      </section>
+      <CalloutComponent :title="calloutStatusTitle" :message="calloutStatusMessage" />
+      <!-- :iconHref="'../assets/icons/exclamation.svg#exclamation'"
+        :iconColor="'#ff6347'" -->
+      <section>
+        <h2>How I Work</h2>
+        <p>
+          <em
+            >I care about the entire value stream—from design and development to operations and
+            protection—I approach my work with collaboration, continuous feedback, and knowledge
+            management.</em
+          >
+          These principles are at the heart of my work process, ensuring teams bring ideas to life
+          together while delivering solutions that are impactful, reliable, and secure.
+        </p>
+        <h3>Feel - Empathize</h3>
+        <p>
+          I <em>understand the problem</em> by connecting with the people involved. Grasp their
+          needs, frustrations, and goals.
+        </p>
+        <h3>Think - Analyse</h3>
+        <p>
+          I dig deep to <em>uncover the root cause</em> and go beyond the symptoms. Define the
+          challenge at its core and plan the next steps.
+        </p>
+        <h3>Imagine - Create - Design</h3>
+        <p>
+          I <em>find solutions</em> by thinking beyond limits and conceptualizing tangible designs.
+          I test, refine and iterate to turn ideas into real-world solutions.
+        </p>
+        <h3>Do - Execute - Development</h3>
+        <p>
+          I make a plan to make it happen and <em>build the solution.</em> I test, refine, and
+          iterate to ensure continuous and optimized value delivery.
+        </p>
+      </section>
+      <section>
+        <h2>My Default Settings</h2>
+        <h3>Core Competencies</h3>
+        <ul>
+          <li><b>Problem-Solving:</b> Lean Philosophy, Design Thinking, Double Diamond</li>
+          <li><b>Development Processes:</b> Agile Software Development Life Cycle (SDLC)</li>
+          <li><b>Programming Principles:</b> Object-Oriented Programming (OOP)</li>
+          <li>
+            <b>Collaboration:</b> Empathetic communication to bridge gaps between design,
+            development, operations, and security.
+          </li>
+          <li>
+            <b>Personal Knowledge Management:</b> Enables me to write clear, structured
+            documentation to support knowledge sharing.
+          </li>
+        </ul>
+        <h3>Technical Tools</h3>
+        <ul>
+          <li><b>Frontend:</b> HTML, CSS, JavaScript, Vue.js</li>
+          <li><b>Backend:</b> Node.js, MongoDB</li>
+          <li><b>Build & Deployment:</b> Webpack, npm, Docker</li>
+          <li><b>Version Control:</b> Git (Trunk-Based Development Workflow)</li>
+          <li><b>Design & Prototyping:</b> Figma</li>
+          <li><b>Project Management:</b> Jira</li>
+          <li><b>Operating Systems:</b> Linux (Command Line Proficiency)</li>
+        </ul>
+        <h3>Things I Want to Learn Soon</h3>
+        <ul>
+          <li>
+            <b>Automation:</b> CI/CD Pipelines, Infrastructure as Code (IaC) with tools like
+            Ansible, Terraform, or GitHub Actions.
+          </li>
+          <li>
+            <b>Monitoring & Observability:</b> Understanding of system monitoring, logging, and
+            observability tools like Prometheus, Grafana, and ELK Stack.
+          </li>
+          <li>
+            <b>Security Awareness:</b> Applying secure development practices, DevSecOps and tools
+            like Snyk or OWASP principles.
+          </li>
+          <li>And much more...</li>
+        </ul>
+      </section>
+      <section>
+        <h2>The Right Fit for Me</h2>
+        <ul>
+          <li>
+            <b>Remote in Europe</b>
+            <p>
+              My ideal workplace is fully remote, with my closest collaborators ideally based in or
+              near the Central European Time zone. This allows for easy communication and better
+              alignment in work schedules.
+            </p>
+          </li>
+          <li>
+            <b>Kind and Collaborative Team</b>
+            <p>
+              I thrive in environments that prioritize trust, collaboration, and kindness, where
+              teams work together with mutual respect and a commitment to shared goals.
+            </p>
+          </li>
+          <li>
+            <b>Continuous Learning Enabled</b>
+            <p>
+              Continuous learning and growth are essential to me. I value workplaces where
+              mentorship, resource sharing, and opportunities for skill development are encouraged,
+              fostering both personal and professional evolution.
+            </p>
+          </li>
+          <li>
+            <b>Fair and Transparent Compensation</b>
+            <p>
+              A fair and competitive salary is essential, reflecting the value of my skills,
+              contributions, and the cost of living in Europe. I appreciate organizations that
+              approach compensation with transparency and equity, ensuring mutual respect and trust.
+            </p>
+          </li>
+          <li>
+            <b>Ethical and Sustainable Organization</b>
+            <p>
+              I am deeply committed to working only for ethical companies whose values align with
+              mine. I place high value on data privacy and responsible use of technology, ensuring
+              the rights and well-being of individuals are respected.
+            </p>
+          </li>
+        </ul>
+      </section>
+      <CalloutComponent :title="calloutConnectTitle" :message="calloutConnectMessage" />
+    </main>
+    <FooterComponent />
+  </div>
+</template>
+
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  /* align-items: center; */
+  align-self: center;
+  gap: 1.5rem;
+  max-width: 40rem;
+  margin: 3rem 0;
+  padding: 0 1rem;
+  overflow-y: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  p,
+  ul {
+    font-size: 1.125rem;
+    font-weight: 500;
+    line-height: 2rem;
+    margin-bottom: 1em;
+  }
+  button {
+    font-size: 1.125rem;
+    font-weight: 500;
+    line-height: 1.5rem;
+    padding: 0.75rem 1.5rem;
+    border: none;
+    border-radius: 0.5rem;
+    background-color: var(--button-background);
+    color: var(--button-text);
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  ul,
+  li {
+    list-style-type: disc;
+    margin-left: 0.5rem;
+    padding-left: 0rem;
+  }
+  ul {
+    margin-top: 0.5rem;
+    margin-bottom: 2.5rem;
+  }
+}
+
+.icon {
+  margin-right: 8px;
+  color: var(--icon-color, #6a2e2e); /* Use the iconColor prop here */
+}
+
+svg {
+  use {
+    fill: currentColor;
+  }
+}
+
+/* section {
+  width: 100%;
+} */
+</style>
---
+++ src/components/CalloutComponent.vue
@@ -0,0 +1,84 @@
+// CalloutComponent.vue
+<template>
+  <section class="callout">
+    <div class="callout-header">
+      <!-- <svg class="icon" width="24px" height="24px" aria-hidden="true">
+        <use href="../assets/icons/exclamation.svg#exclamation" />
+      </svg> -->
+      <h3>{{ title }}</h3>
+    </div>
+    <p>{{ message }}</p>
+    <router-link to="/contact" active-class="active" aria-label="Contact me page">
+      Contact Me
+    </router-link>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CalloutComponent',
+  props: {
+    title: {
+      type: String,
+      default: 'Callout Title',
+    },
+    message: {
+      type: String,
+      default: 'Callout Descriptive Message',
+    },
+    // iconHref: {
+    //   type: String,
+    //   default: '../assets/icons/mail.svg#mail',
+    // },
+    // iconColor: {
+    //   type: String,
+    //   default: '#333333',
+    // },
+  },
+}
+</script>
+
+<style scoped>
+.callout {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  background-color: #f3f8fd;
+  border: 3px solid #3c7bbe;
+  border-radius: 0.5rem;
+  padding: 1.5rem;
+  .callout-header {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+}
+
+/* .icon {
+  margin-right: 8px;
+  color: var(--icon-color, #6a2e2e); 
+}
+
+svg {
+  use {
+    stroke: currentColor;
+  }
+} */
+
+h3 {
+  font-style: italic;
+}
+
+a {
+  display: inline-block;
+  text-decoration: none;
+  background-color: rgb(234, 186, 12);
+  text-align: center;
+  padding: 0.75rem;
+  border-radius: 0.5rem;
+  &:hover {
+    cursor: pointer;
+    filter: brightness(105%);
+  }
+}
+</style>
