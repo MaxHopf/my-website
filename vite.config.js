e5be0e0c0ffe886754338b70897acc7cfae3ea94
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2024-12-22 00:02:07 +0200

    Remove base path configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  build: {
-    outDir: 'dist',
   },
 })
