ac62aed555cc0ebf9c6499e9e7602819add74bf8
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2024-12-21 23:53:07 +0200

    Reset base URL in router.js

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('https://maxhopf.github.io/my-website/'),
+  history: createWebHistory('/my-website/'),
   routes,
 })
 
