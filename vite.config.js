4b6e0e41dadbcbdf5c1dd86a5e42074eaee3a7e9
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2024-12-22 00:11:34 +0200

    Set base path in vite.config.js to '/my-website/'

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  base: '/my-website/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 })
